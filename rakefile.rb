3c0337b9a1d8c505673af9ee6d8d84862039fa19
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-03-15 14:00:53 +0530

    Directory changes

--- rakefile.rb
+++ rakefile.rb
@@ -12,13 +12,13 @@ namespace :resume do
 			Dir.mkdir("resume/css") unless Dir.exists?("resume/css")
 			Dir.mkdir("resume/img") unless Dir.exists?("resume/img")
 
-			@config = YAML.load_file "resume.yml"
+			@config = YAML.load_file "config/resume.yml"
 			@resume = ERB.new File.open("views/template1/resume.erb").read
 			File.write "resume/resume.html", @resume.result
 
 			sh "cp views/template1/css/resume.css resume/css"
 			sh "cp views/bootstrap/bootstrap.min.css resume/css"
-			sh "cp profile.jpg resume/img"
+			sh "cp config/profile.jpg resume/img"
 
 			puts ""
 			puts "Resume created successfully. See 'resume/resume.html'"
