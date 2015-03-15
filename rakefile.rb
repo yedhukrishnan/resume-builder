8eb27ec1de27955b9c9720307ce216f32c8960ce
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-03-15 11:06:07 +0530

    Update template, add profile photo

--- rakefile.rb
+++ rakefile.rb
@@ -8,11 +8,13 @@ namespace :resume do
 
 		Dir.mkdir("resume") unless Dir.exists?("resume")
 		Dir.mkdir("resume/css") unless Dir.exists?("resume/css")
+		Dir.mkdir("resume/img") unless Dir.exists?("resume/img")
 		@config = YAML.load_file "resume.yml"
 		@resume = ERB.new File.open("views/resume.erb").read
 		File.write "resume/resume.html", @resume.result
 		sh "cp views/template1/css/resume.css resume/css"
 		sh "cp views/bootstrap/bootstrap.min.css resume/css"
+		sh "cp profile.jpg resume/img"
 	end
 
 end
